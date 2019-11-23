5c714bfc71a26519be9fab8b9877bdde871ad41a
nate <nate@localhost.localdomain>
2019-11-23 17:15:44 -0500

    learning functions

--- violent_python/chap1_connect2socket.py
+++ violent_python/chap1_connect2socket.py
@@ -2,13 +2,28 @@
 
 import socket
 
-socket.setdefaulttimeout(2)
-s = socket.socket()
-ip = '127.0.0.1'
-port = '21'
+def retBanner(ip, port):
+     try:
+         socket.setdefaulttimeout(2)
+         s = socket.socket()
+         s.connect((ip, port))
+         banner = s.recv(1024)         
+         return banner
+     except Exception as e:
+         print('[-] Error' + str(e))
 
-try:
-    s.connect((str(ip),int(port)))
-except Exception as e:
-    print('[-] Error = ' + str(e))
+def main():
+    ip1 = '127.0.0.1'
+    ip2 = '192.168.101.129'
+    port = 22
+    
+    banner1 = retBanner(ip1, port)
+    banner2 = retBanner(ip1, port)
 
+    if banner1:
+       print('[+] ' + str(ip1) + ': ' + str(banner1))
+    if banner2: 
+       print('[+] ' + str(ip2) + ': ' + str(banner2))
+
+if __name__ == '__main__':
+  main()
