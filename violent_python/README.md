890ea3b7f2ecc0d56e8dc97818d32f5f27dcd9e0
nate <nate@localhost.localdomain>
2019-11-23 17:57:46 -0500

    more

---
+++ violent_python/README.md
@@ -0,0 +1,5 @@
+# Things to improve:
+
+If running a scan against a range, add a block of code to see which IP addresses are able to be hit and which aren't. It would be nice to have that so that there are not a bunch of `[-] Error Timed out` errors.
+
+Thinking about it though, what if the org blocks ICMP. Do I just need to suck it up and roll with a bunch of `[-] Error Timed out`'s?
--- violent_python/chap1_connect2socket.py
+++ violent_python/chap1_connect2socket.py
@@ -10,20 +10,23 @@ def retBanner(ip, port):
          banner = s.recv(1024)         
          return banner
      except Exception as e:
-         print('[-] Error' + str(e))
+         print('[-] Error ' + str(e))
 
 def main():
-    ip1 = '127.0.0.1'
-    ip2 = '192.168.101.129'
-    port = 22
-    
-    banner1 = retBanner(ip1, port)
-    banner2 = retBanner(ip1, port)
+    portList = [21,22,25,53,80,443,3389] 
+    for x in range(1, 255):
+      ip = '192.168.101.' + str(x)
+      for port in portList:
+          banner = retBanner(ip, port)
+          if banner:
+              print('[+] ' + ip + ': ' + banner)
 
-    if banner1:
-       print('[+] ' + str(ip1) + ': ' + str(banner1))
-    if banner2: 
-       print('[+] ' + str(ip2) + ': ' + str(banner2))
-
+# the below link explains pretty well why you need and would want the below code
+# https://www.geeksforgeeks.org/what-does-the-if-__name__-__main__-do/
 if __name__ == '__main__':
   main()
+
+####
+# Things to improve:
+# if running a scan against a range, add a block of code to see which IP addresses are
+# able to be hit and which aren't. I don't actually know if that is a good idea or not
