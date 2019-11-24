fc301a8544d43c2454887f1cba44b7cb8ee048ec
nate <dreier.nate@gmail.com>
2019-11-24 16:16:35 -0500

    chapter 2, done

---
+++ automate_the_boring_stuff/chap2_randomPrograms.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3.6
+
+# There was no one project for chapter 2, here are a bunch of little tests that I ran
+# based on the reading.
+
+'''
+name = ''
+while not name:
+	print('hello')
+	name = input()
+guests = 0
+while not guests:
+	guests = input()
+	print('you will have ' + guests + ' guests')
+'''
+
+'''
+for i in range(0, 20, 2):
+	print(i)
+'''
+
+'''
+import random
+num = 0
+for num in range(10):
+	print(random.randint(1, 69))
+'''
+
+
+'''
+spam = 3
+if spam == 0:
+	print('spam is ham')
+elif spam == 2:
+	print('spam is slam')
+else:
+	print('spam is spam')
+'''
+
+'''
+
+import sys
+
+while True:
+	print('type exit to exit')
+	response = input()
+	if response == 'exit':
+		sys.exit()
+	print('you typed ' + response)
+'''
