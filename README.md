b946852565d3fd4b3888caec48dcb00d8cb848a6
nate <dreier.nate@gmail.com>
2019-11-24 19:09:39 -0500

    mer commits

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Tryna learn some Python
 
 naming convention is `chapter#_programName.py`
 
+## Challenges
+First challenge to work up to is FizzBuzz, print the numbers one through thirty. If a number is divisible by 3, instead print Fizz if a number is divisible by 5, print Buzz.
+
 ## notes
 
 The goal of this is to get a better grasp on Python, learn best practices, formatting and more. I hope to improve my Python as well as git best practices, starting off with the README.
--- violent_python/chap1_connect2socket.py
+++ violent_python/chap1_connect2socket.py
@@ -3,23 +3,31 @@
 import socket
 
 def retBanner(ip, port):
-     try:
-         socket.setdefaulttimeout(2)
-         s = socket.socket()
-         s.connect((ip, port))
-         banner = s.recv(1024)         
-         return banner
-     except Exception as e:
-         print('[-] Error ' + str(e))
+	 try:
+		 socket.setdefaulttimeout(2)
+		 s = socket.socket()
+		 s.connect((ip, port))
+		 banner = s.recv(1024)
+		 return banner
+	 except Exception as e:
+		 print('[-] Error ' + str(e))
+
+def checkVulns(banner):
+	f = open('vuln_banners.txt','rb')
+	for line in f.readlines():
+		if line.strip('b\n') in banner:
+			print('[+] Server is vulnerable: ' + banner.strip('\n'))
+
 
 def main():
-    portList = [21,22,25,53,80,443,3389] 
-    for x in range(1, 255):
-      ip = '192.168.101.' + str(x)
-      for port in portList:
-          banner = retBanner(ip, port)
-          if banner:
-              print('[+] ' + ip + ': ' + banner)
+	portList = [22]
+	for x in range(127, 130):
+		ip = '192.168.101.' + str(x)
+		for port in portList:
+			banner = retBanner(ip, port)
+			if banner:
+				print('[+] ' + str(ip) + ': ' + str(banner))
+				checkVulns(banner)
 
 # the below link explains pretty well why you need and would want the below code
 # https://www.geeksforgeeks.org/what-does-the-if-__name__-__main__-do/
