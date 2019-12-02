80fbd0b690047add5aa70bf0dba39f1bb0d6dd16
nate <dreier.nate@gmail.com>
2019-12-01 21:38:27 -0500

    I swear, it's a work in progress

---
+++ violent_python/chap1_passwordCracker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.6
+
+import crypt
+import sys
+import os
+
+def check_file():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print('[-] ' + filename + ' does not exist.')
+            exit(0)
+        if not os.access(filename, os.R_OK):
+            print('[-] ' + filename + ' access denied.')
+            exit(0)
+
+def hack_a_pass(passpass):
+
+def main():
+    file = check_file()
+    f = open(filename, r)
+    
+        
