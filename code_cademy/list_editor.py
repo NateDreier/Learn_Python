26ebc956e65c609781f9fa55127970a37b61a44b
nate <dreier.nate@gmail.com>
2020-01-21 21:34:35 -0500

    adding additional function

--- code_cademy/list_editor.py
+++ code_cademy/list_editor.py
@@ -9,4 +9,15 @@ def list_editor(lst, index):
         new_list.append(lst[-1])
         return(new_list)
 
+def remove_middle(lst, start, end):
+    if start >= len(lst) or end >= len(lst):
+        return(lst)
+    else:
+        new_list = lst[0:start]
+        new_list = new_list + lst[end+1:]
+        print(new_list)
+        return(new_list)
+    
+
 print(list_editor([1, 2, 3, 4, -12, 6], 4))
+print(remove_middle([1, 2, 3, 4 ,22, 34, 5], 1, 3))
