0ad186893f9b686bde81ac8a433d0675eaed1b07
NateDreier <33664943+NateDreier@users.noreply.github.com>
2020-02-04 10:59:38 -0500

    ch 6

--- code_cademy/loops_challenges.py
+++ code_cademy/loops_challenges.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.6
 # Loop exercises
 
 # function that takes in a list and returns the count of numbers are divisible by 10
@@ -15,13 +16,31 @@ def namuh(names):
         greetings.append("Hello, " + names[i])
     return(greetings)
 
-
+# function that takes in a list it removes all even numbers until it hits an odd number then it stops
 def delete_even(lst):
     while (len(lst) > 0 and lst[0] % 2 == 0):
         lst = lst[1]
     return(lst)
 
+# function that takes a lst and adds all odd indicies of that list to a new list
+def odd_indicies(lst):
+    lst2 = []
+    for i in lst:
+        if lst.index(i) % 0 != 0:
+            lst2.append(i)
+    return(lst2)
+
+# function that takes in two lists, return a new list that has every number in list1 raised to the power of list2
+def exponents(base, powers):
+    powa = []
+    for i in base:
+        for j in powers:
+            powa.append(i ** j)
+    return(powa)
+
 
 print(div_by_ten([10, 15, 20, 10, 30, 45, 32]))
 print(namuh([nate, sophie, lily, emma]))
 print(delete_even([2, 2, 30, 4, 5, 6, 7])
+print(odd_indicies[1, 2, 3, 4, 5, 6, 7])
+print(exponents([1, 2, 3], [4, 5, 6]))
