4b851c2b08bbf18c82e0fd29cd29ed39abf4f49d
nate <my@email.nate>
2020-03-29 11:12:52 -0700

    more

--- oh-riley/chap1_pt2.py
+++ oh-riley/chap1_pt2.py
@@ -23,5 +23,27 @@ beevus = {'name': 'beevus head', 'age': 20, 'pay': 50000, 'job': 'dev'}
 butt = {'name': 'butt head', 'age': 69, 'pay': 100000, 'job': 'nuthing'}
 nate = {'name': 'nate washer', 'age': 12, 'pay': 60000, 'job': 'wannabedev'}
 
-print(butt['name'], nate['name'])
-print(nate['name'].split()[-1])
+peeps_dict = [beevus, butt, nate]
+
+#print(butt['name'], nate['name'])
+#print(nate['name'].split()[-1])
+
+for i in peeps_dict:
+    print(i['name'], i['pay'], sep=', ')
+
+names = [i['name'] for i in peeps_dict]
+#print(names)
+#print(sum(i['pay'] for i in peeps_dict))
+
+# Nested dicts
+
+beevus2 = {'name': {'first': 'beevus', 'last': 'head'},
+           'age': 32,
+           'job': ['wash', 'up'],
+           'pat': (30000, 40000)}
+butt2 = {'name': {'first': 'butt', 'last': 'head'},
+           'age': 69,
+           'job': ['wash', 'up'],
+           'pat': (60000, 20000)}
+peeps_nest = [beevus2, butt2]
+#print(beevus2['name'], beevus2['name']['last'])
