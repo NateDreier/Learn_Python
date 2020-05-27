4cb641a358fbfcc6eedc0c26d72aeb35f44b88f0
n8 <bamboozal@insurance.kek>
2020-05-26 17:54:47 -0700

    improvements

--- dc_pull.py
+++ dc_pull.py
@@ -2,37 +2,47 @@
 
 import sys
 import os
-# https://docs.python.org/3/library/concurrent.futures.html
-import concurrent.futures
-# https://docker-py.readthedocs.io/en/stable/
-import docker
+import os.path
+from os import path
+import concurrent.futures # https://docs.python.org/3/library/concurrent.futures.html
+import docker # https://docker-py.readthedocs.io/en/stable/
 
-cli = docker.APIClient(base_url='unix://var/run/docker.sock')
+cli = docker.APIClient(base_url="unix://var/run/docker.sock")
 current_dir = os.getcwd()
 repository = sys.argv[2]
 
-def the_whole_shebang(image):
-  img_t = image.split(":")
-  img = img_t[0].strip()
-  t = img_t[1].strip()
 
-  print(f'Pulling, retagging, saving and rmi\'ing: {img}:{t}')
-  # Pulls the container
-  cli.pull(f'{img}:{t}')
-  # Tags the container with the new tag
-  cli.tag(f'{img}:{t}', f'{repository}/{img}', f'{t}')
-  
-  # Next 5 lines of code tar up the containers
-  f = open(f'{current_dir}/move/{img}{t}.tar', 'wb')
-  im = cli.get_image(f'{repository}/{img}:{t}')
-  for chunk in im:
-    f.write(chunk)
-  f.close
-  # Deletes all downloaded images
-  for i in cli.images():
-    cli.remove_image(i,force=True)    
+if path.exists(os.path.join(current_dir, "move")) is not True:
+        os.mkdir(os.path.join(current_dir, "move"))
+
+
+def the_whole_shebang(image):
+    img_t = image.split(":")
+    img = img_t[0].strip()
+    t = img_t[1].strip()
+
+    print(f"Pulling, retagging, saving and rmi'ing: {img}:{t}")
+    # Pulls the container
+    cli.pull(f"{img}:{t}")
+    # Tags the container with the new tag
+    cli.tag(f"{img}:{t}", f"{repository}/{img}", f"{t}")
+
+    
+    new_image_name = f"{img}{t}.tar"
+    im = cli.get_image(f"{repository}/{img}:{t}")
+    
+
+    with open(os.path.join(current_dir, "move", new_image_name), 'wb+') as f:
+        for chunk in image:
+            f.write(chunk)
+
+    
+    # Deletes all downloaded images
+#    for i in cli.images():
+#    cli.remove_image(img_t, force=True)
+
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
-  f = open(sys.argv[1], "r")
-  lines = f.readlines()
-  executor.map(the_whole_shebang, lines)
+    f = open(sys.argv[1], "r")
+    lines = f.readlines()
+    executor.map(the_whole_shebang, lines)
