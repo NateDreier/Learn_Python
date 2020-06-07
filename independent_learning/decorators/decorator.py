bf95bb0cfc8a74735eba8e7982e95ad119039562
n8 <bamboozal@insurance.kek>
2020-06-07 14:29:16 -0700

    failed attempt at classes

--- independent_learning/decorators/decorator.py
+++ independent_learning/decorators/decorator.py
@@ -6,65 +6,72 @@ Playing with Decorators
 2. Decorating decorator to decorate the output.
 3. Number checking decorator to check the validity of the number. 
 """
-
 import time
 import concurrent.futures
 
 
-def decoratorator(func):
-    def p(*args):
-        func(*args)
-        print("*" * 72)
-        print("DONE")
+class Dec: 
+    def __init__ (self, n):
+        self.n = n
 
-    return p
+    def decoratorator(func):
+        def p(*args):
+            func(*args)
+            print("*" * 72)
+            print("DONE")
 
+        return p
 
-def timerator(func):
-    def f(*args):
-        before = time.perf_counter()
-        rv = func(*args)
-        after = time.perf_counter()
-        print(f"Elapsed time: {round(after-before, 2)} ")
+    def timerator(func):
+        def f(*args):
+            before = time.perf_counter()
+            rv = func(*args)
+            after = time.perf_counter()
+            print(f"Elapsed time: {round(after-before, 2)} ")
 
-    return f
+        return f
 
+    def test_nat_num(func):
+        def helper(n):
+            if type(n) == int and n > 0:
+                return func(n)
+            else:
+                raise Exception("Argument is not an integer")
 
-def test_nat_num(func):
-    def helper(n):
-        if type(n) == int and n > 0:
-            return func(n)
+        return helper
+
+    @test_nat_num
+    def factorial(n):
+        if n == 1:
+            return 1
         else:
-            raise Exception("Argument is not an integer")
-    return helper
+            return n * factorial(n - 1)
 
-@test_nat_num
-def factorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    # print(factorial(4))
 
-#print(factorial(4))
-
-@decoratorator
-@timerator
-def iter(*args):
-    time.sleep(*args)
-
-    return f"Done sleeping for {args} seconds."
-
-
-if __name__ == "__main__":
+    @decoratorator
     @timerator
-    def thread_test():
+    def iter(n):
+        print(n)
+        time.sleep(n)
+
+        return f"Done sleeping for {n} seconds."
+
+    @timerator
+    def thread_test(self):
         print("*" * 72)
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            secs = [.5, 1, .3, 2]
-            [executor.submit(iter, sec) for sec in secs]
+            secs = self.n # [0.5, 1, 0.3, 2]
+            #[executor.submit(iter, sec) for sec in secs]
+            results = executor.submit(iter, .5)
+            print(results.result())
         print("_" * 72)
 
-    thread_test()
-
-    print("\nRunning the factorial function")
-    print(factorial(4))
+a = Dec([1, 2, 3])
+b = Dec(3)
+print(a.thread_test())
+#print(a.thread_test())
+#print(Dec.thread_test([1, 2, 3]))
+# thread_test()
+# print("\nRunning the factorial function")
+# print(factorial(4))
