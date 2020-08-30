4fdfcb858d308e83434f8d3ec1982382646cd13d
nate <dreier.nate@gmail.com>
2020-08-30 11:47:52 -0700

    adding in type hinting

--- independent_learning/random_proj/lazy_prime.py
+++ independent_learning/random_proj/lazy_prime.py
@@ -3,15 +3,15 @@
 
 how_many_prime = int(input("How many prime numbers would you like to see? "))
 
-def nat(n):
-    yield n
-    yield from nat(n+1)
+def natural_num(num: int)-> int:
+    yield num
+    yield from natural_num(num+1)
 
-def sieve(s):
+def sieve(s: int)-> int:
     n = next(s)
     yield n
     yield from sieve(i for i in s if i%n!=0)
 
-p = sieve(nat(2))
+p = sieve(natural_num(2))
 for _ in range(how_many_prime):
     print(next(p))
