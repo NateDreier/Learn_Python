17f339575dcba7eca6bf5ab7970058a7d04070ba
n8 <bamboozal@insurance.kek>
2020-11-27 21:37:59 -0800

    adding error codes

--- api_python/proj2/api.py
+++ api_python/proj2/api.py
@@ -6,14 +6,26 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+technologies = []
 
-class Student(Resource):
+# 404 NOT FOUND
+# 201 CREATED
+# 202 ACCEPTED (delaying the creation)(ie the creation takes a couple of minutes)
+
+
+class Tech(Resource):
     def get(self, name):
-        return {"studen": name}
+        for tech in technologies:
+            if tech["name"] == name:
+                return tech
+        return {"item": None}, 404
+
+    def post(self, name):
+        item = {"name": name, "cool factor": 1000}
+        technologies.append(item)
+        return item, 201
 
 
-api.add_resource(
-    Student, "/student/<string:name>"
-)  # https://127.0.0.1:5000/student/some_name
+api.add_resource(Tech, "/tech/<string:name>")  # https://127.0.0.1:5000/tech/some_tech
 
 app.run(debug=True, host="0.0.0.0")
