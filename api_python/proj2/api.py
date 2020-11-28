ae6cd56b24d3b3bb6f7c5f70a8a98ff2d24e08ce
n8 <bamboozal@insurance.kek>
2020-11-27 20:25:42 -0800

    adding basic Student get class

--- api_python/proj2/api.py
+++ api_python/proj2/api.py
@@ -5,3 +5,11 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+class Student(Resource):
+    def get(self, name):
+        return {'studen': name}
+
+api.add_resource(Student, '/student/<srting:name>') # https://127.0.0.1:5000/student/some_name
+
+app.run(debug=True, host="0.0.0.0")
